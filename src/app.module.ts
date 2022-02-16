028dd61c034d89a078d192bae20cbbf60022bd2f
AofPatiphan <patiphan.ma@hotmail.com>
2022-02-16 19:38:15 +0700

    add config postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,23 +6,28 @@ import { HarrypotterModule } from './harrypotter/harrypotter.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoModule } from './todo/todo.module';
 import { Todo } from './todo/entities/todo.entity';
+import { ConfigModule } from '@nestjs/config';
+import { configService } from './config/configService';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     HarrypotterModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'P@ssw0rd',
-      database: 'flutterapi',
-      entities: [Harrypotter, Todo],
-      synchronize: true,
-    }),
     TodoModule,
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: process.env.DATABASE_HOST,
+    //   port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    //   username: process.env.DATABASE_USER,
+    //   password: process.env.DATABASE_PASSWORD,
+    //   database: process.env.DATABASE_DBNAME,
+    //   entities: [Harrypotter, Todo],
+    //   synchronize: true,
+    // }),
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+// 1.47.20.244
